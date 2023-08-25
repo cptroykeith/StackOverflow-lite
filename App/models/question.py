833ef3e5c81c4f17292ed15ca06228c4b6ecccdf
your-name <katongole.roy100@gmail.com>
2023-08-25 23:27:23 +0300

    get all question added

--- App/models/question.py
+++ App/models/question.py
@@ -6,3 +6,8 @@ class Question:
         
     @classmethod
     def get_all(cls):
+        # Replace with your actual logic to fetch questions from a data source
+        return [
+            cls(1, "Sample Question 1", "This is the content of question 1."),
+            cls(2, "Sample Question 2", "This is the content of question 2."),
+        ]
